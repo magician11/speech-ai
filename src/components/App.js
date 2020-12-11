204920a4944597b7131cca17a8f4f08e9be3f4ad
Andrew Golightly <support@andrewgolightly.com>
2020-12-11 20:56:03 +1300

    better state feedback

--- src/components/App.js
+++ src/components/App.js
@@ -10,41 +10,40 @@ import {
   Box,
   Grid
 } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import { say } from '../modules/speech';
 import { translate } from '../modules/translate';
 import speakerImage from '../images/speaker.jpg';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    backgroundImage: `url(${speakerImage})`,
-    backgroundSize: 'cover',
-    height: window.innerHeight
-  },
-  buttonContainer: {
-    textAlign: 'center',
-    marginTop: theme.spacing(3)
-  }
-}));
-
 const App = () => {
-  const classes = useStyles();
-  const [speaking, setSpeaking] = useState(false);
   const [text, setText] = useState();
+  const [analysingText, setAnalysingText] = useState(false);
+  const [readyToSpeak, setReadyToSpeak] = useState(false);
+
+  const readyToPlay = () => setReadyToSpeak(true);
 
   const sayText = async () => {
-    setSpeaking(true);
+    setAnalysingText(true);
 
     // https://stackoverflow.com/a/6259543/2813041
     var parts = text.match(/[\s\S]{1,2200}/g) || [];
 
     for (const part of parts) {
       const englishText = await translate(part);
-      await say(englishText);
+      await say(englishText, readyToPlay);
+      setReadyToSpeak(false);
     }
 
-    setSpeaking(false);
+    setAnalysingText(false);
   };
+
+  let status;
+  if (readyToSpeak) {
+    status = 'Let me read this back to you...';
+  } else if (analysingText) {
+    status = 'Analysing the text...';
+  } else {
+    status = "Enter some text when you're ready...";
+  }
 
   return (
     <Fragment>
@@ -61,33 +60,58 @@ const App = () => {
           </Typography>
         </Toolbar>
       </AppBar>
-
-      <Grid
-        container
-        className={classes.root}
-        justify="center"
-        alignItems="center"
+      <div
+        style={{
+          backgroundImage: `url(${speakerImage})`,
+          backgroundSize: 'cover',
+          height: window.innerHeight,
+          paddingTop: 88
+        }}
       >
-        <Grid item xs={10} sm={8} md={6}>
-          <Paper elevation={3}>
-            <TextField
-              fullWidth
-              autoFocus
-              onChange={event => setText(event.target.value)}
-              value={text}
-              multiline
-              rows={11}
-              placeholder="What would you like me to say?"
-              variant="filled"
-            />
-          </Paper>
-          <Box className={classes.buttonContainer}>
-            <Button variant="contained" onClick={sayText} disabled={speaking}>
-              Read it to me
-            </Button>
-          </Box>
+        <Grid
+          container
+          justify="center"
+          alignItems="center"
+          style={{ marginTop: 33, marginBottom: 22 }}
+        >
+          <Grid item xs={8} sm={6} md={4} lg={3}>
+            <Paper style={{ padding: 11 }}>
+              <Typography variant="body2">{status}</Typography>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
+        <Grid container justify="center" alignItems="center">
+          <Grid item xs={10} sm={8} md={6}>
+            <Paper elevation={3}>
+              <TextField
+                fullWidth
+                autoFocus
+                disabled={analysingText}
+                onChange={event => setText(event.target.value)}
+                value={text}
+                multiline
+                rows={11}
+                placeholder="What would you like me to say?"
+                variant="filled"
+              />
+            </Paper>
+            <Box
+              style={{
+                textAlign: 'center',
+                marginTop: 33
+              }}
+            >
+              <Button
+                variant="contained"
+                onClick={sayText}
+                disabled={analysingText}
+              >
+                Read it to me
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </div>
     </Fragment>
   );
 };
--- src/modules/speech.js
+++ src/modules/speech.js
@@ -1,15 +1,22 @@
 import firebase from './firebase';
 
-const say = async text => {
+const say = async (text, readyToPlay) => {
   return new Promise(async (resolve, reject) => {
+    console.log('about to get speech...');
     var textToSpeech = firebase.functions().httpsCallable('textToSpeech');
     const res = await textToSpeech({ text });
+    // console.log('got speech...');
     // https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement/Audio
     const speech = new Audio('data:audio/wav;base64,' + res.data.audioContent);
     speech.addEventListener('ended', () => {
+      // console.log('all done playing.');
       resolve();
     });
-    speech.play();
+    speech.addEventListener('canplaythrough', event => {
+      console.log('about to start playing...');
+      readyToPlay();
+      speech.play();
+    });
   });
 };
 
