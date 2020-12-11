ab4807de5cce14e2b3ac26d262a3a2611f71c27f
Andrew Golightly <support@andrewgolightly.com>
2020-12-12 11:54:34 +1300

    added iPhone check

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import {
   Typography,
   CssBaseline,
@@ -18,6 +18,16 @@ const App = () => {
   const [text, setText] = useState();
   const [analysingText, setAnalysingText] = useState(false);
   const [readyToSpeak, setReadyToSpeak] = useState(false);
+  const [isIphone, setIsIphone] = useState(false);
+  const [device, setDevice] = useState();
+
+  useEffect(() => {
+    setDevice(navigator.platform);
+
+    if (navigator.platform === 'iPhone') {
+      setIsIphone(true);
+    }
+  }, []);
 
   const readyToPlay = () => setReadyToSpeak(true);
 
@@ -45,6 +55,74 @@ const App = () => {
     status = "Enter some text when you're ready...";
   }
 
+  const dashboard = isIphone ? (
+    <Grid
+      container
+      justify="center"
+      alignItems="center"
+      style={{ marginTop: 33 }}
+    >
+      <Grid item xs={12} sm={6} lg={4}>
+        <Paper style={{ padding: 22, margin: 11 }}>
+          <Typography variant="h6" gutterBottom>
+            Sorry, this app doesn't work with iPhones.
+          </Typography>
+          <Typography variant="body1">
+            It'll work on your computer though, even Macs 😀
+          </Typography>
+        </Paper>
+      </Grid>
+    </Grid>
+  ) : (
+    <>
+      <Grid
+        container
+        justify="center"
+        alignItems="center"
+        style={{ marginTop: 33, marginBottom: 22 }}
+      >
+        <Grid item xs={10} sm={6} md={4} lg={3}>
+          <Paper style={{ padding: 11 }}>
+            <Typography variant="body2" gutterBottom>
+              {status}
+            </Typography>
+          </Paper>
+        </Grid>
+      </Grid>
+      <Grid container justify="center" alignItems="center">
+        <Grid item xs={10} sm={8} md={6}>
+          <Paper elevation={3}>
+            <TextField
+              fullWidth
+              autoFocus
+              disabled={analysingText}
+              onChange={event => setText(event.target.value)}
+              value={text}
+              multiline
+              rows={11}
+              placeholder="What would you like me to say?"
+              variant="filled"
+            />
+          </Paper>
+          <Box
+            style={{
+              textAlign: 'center',
+              marginTop: 33
+            }}
+          >
+            <Button
+              variant="contained"
+              onClick={sayText}
+              disabled={analysingText}
+            >
+              Read it to me
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </>
+  );
+
   return (
     <Fragment>
       <CssBaseline />
@@ -53,10 +131,14 @@ const App = () => {
           <Typography
             variant="h5"
             style={{
-              color: 'white'
+              color: 'white',
+              flexGrow: 1
             }}
           >
             Speech AI
+          </Typography>
+          <Typography variant="caption" gutterBottom style={{ color: 'white' }}>
+            Your device: {device}
           </Typography>
         </Toolbar>
       </AppBar>
@@ -68,49 +150,7 @@ const App = () => {
           paddingTop: 88
         }}
       >
-        <Grid
-          container
-          justify="center"
-          alignItems="center"
-          style={{ marginTop: 33, marginBottom: 22 }}
-        >
-          <Grid item xs={8} sm={6} md={4} lg={3}>
-            <Paper style={{ padding: 11 }}>
-              <Typography variant="body2">{status}</Typography>
-            </Paper>
-          </Grid>
-        </Grid>
-        <Grid container justify="center" alignItems="center">
-          <Grid item xs={10} sm={8} md={6}>
-            <Paper elevation={3}>
-              <TextField
-                fullWidth
-                autoFocus
-                disabled={analysingText}
-                onChange={event => setText(event.target.value)}
-                value={text}
-                multiline
-                rows={11}
-                placeholder="What would you like me to say?"
-                variant="filled"
-              />
-            </Paper>
-            <Box
-              style={{
-                textAlign: 'center',
-                marginTop: 33
-              }}
-            >
-              <Button
-                variant="contained"
-                onClick={sayText}
-                disabled={analysingText}
-              >
-                Read it to me
-              </Button>
-            </Box>
-          </Grid>
-        </Grid>
+        {dashboard}
       </div>
     </Fragment>
   );
